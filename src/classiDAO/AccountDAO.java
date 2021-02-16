01ce36d293ad6d61cd53515f016f516625c66856
Lorenzo3264 <78816362+Lorenzo3264@users.noreply.github.com>
2021-02-16 17:02:30 +0100

    inserita connessione nella classe AccountDAO

--- src/classiDAO/AccountDAO.java
+++ src/classiDAO/AccountDAO.java
@@ -1,9 +1,22 @@
 package classiDAO;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 import org.postgresql.*;
+
 
 
 public class AccountDAO {
 
+	
+	public AccountDAO() {
+		try {
+			Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/CatenaRist");
+		}catch(Exception e) {
+			
+		}
+	}
+	
 	public boolean fetchAccount() {
 		return false; //da cambiare
 	}
